2294695eecbf4507a524e9b03342025588bb137e
Cole Riggle <coleriggle@gmail.com>
2020-01-18 13:21:37 -0500

    Initial Commit

---
+++ 0hh1/Controller/Menu/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  0hh1
+//
+//  Created by Cole Riggle on 1/18/20.
+//  Copyright © 2020 Cole Riggle. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MenuViewController successfully created")
+        view.backgroundColor = .red
+    }
+}
+
---
+++ 0hh1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0hh1
+//
+//  Created by Cole Riggle on 1/18/20.
+//  Copyright © 2020 Cole Riggle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
