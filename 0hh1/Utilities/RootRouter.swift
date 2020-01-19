19aacef6cf394f952119f49d8ac083afa072755c
Cole Riggle <coleriggle@gmail.com>
2020-01-18 19:54:46 -0500

    Fixed ScenceDelegate window initialization bug

--- 0hh1/Utilities/RootRouter.swift
+++ 0hh1/Utilities/RootRouter.swift
@@ -45,20 +45,25 @@ class RootRouter {
     /** Replaces root view controller. You can specify the replacment animation type.
      If no animation type is specified, there is no animation */
     func setRootViewController(controller: UIViewController, animatedWithOptions: UIView.AnimationOptions?) {
+        
         guard let window = UIApplication.shared.keyWindow else {
             fatalError("No window in app")
         }
+        
         if let animationOptions = animatedWithOptions, window.rootViewController != nil {
             window.rootViewController = controller
             UIView.transition(with: window, duration: 0.33, options: animationOptions, animations: {
             }, completion: nil)
         } else {
+            print(window.screen.nativeBounds)
+            window.backgroundColor = .white
             window.rootViewController = controller
         }
+
     }
     
     func loadMainAppStructure() {
-        
+        print("Loading main app structure")
         setRootViewController(controller: menuVC, animatedWithOptions: nil)
     }
     
